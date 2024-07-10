16fd75bb44bb4f286210ac6b6a672188d782a3ae
Camilledelmon <camille.delmon@gmail.com>
2024-07-10 14:26:40 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue';
+import Menu from './components/Menu.vue';
+</script>
+
+<template>
+  <Menu />
+  <div classname="mycontent">
+    <HelloWorld msg="Vite + Vue" />
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/Menu.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string }>()
+</script>
+
+<template>
+<div id="mymenu" className="sidnav">
+<div><img id="navlogo"  src="/src/images/logo.png" alt=""></img></div>
+<div className="sidnav_separator" ><hr/></div>
+<div className = "menucontent">
+    <div id="Accueil" className="content_block"><p className="content_title"> <a className="content_title" href="/">Accueil </a></p></div>
+    <div id="Association" className="content_block"><p className="content_title">L&#39association</p>
+      <p  className="content_section" ><a className="content_txt" href="game">Présentation</a></p>
+      <p className="content_section"><a className="content_txt">Histoire du club</a></p>
+      <p className="content_section"><a className="content_txt">Membres</a></p>
+    </div>
+    <div id="Club" className="content_block"><p className="content_title">Le Club</p>
+      <p  className="content_section" ><a className="content_txt" href="game">Règlement</a></p>
+      <p className="content_section"><a className="content_txt">Installations</a></p>
+      <p className="content_section"><a className="content_txt">Inscriptions & tarifs</a></p>
+      <p className="content_section"><a className="content_txt">Enseignements</a></p>
+    </div>
+    <div id="Rencontres" className="content_block"><p className="content_title">Les Rencontres</p>
+      <p  className="content_section" ><a className="content_txt" href="game">Tournois</a></p>
+      <p className="content_section"><a className="content_txt">Installations</a></p>
+      <p className="content_section"><a className="content_txt">Inscriptions & tarifs</a></p>
+      <p className="content_section"><a className="content_txt">Enseignements</a></p>
+    </div>
+</div>
+</div>
+</template>
